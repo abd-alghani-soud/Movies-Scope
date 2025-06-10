fd0c213eb423b257380b14b16eba7152ab837938
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-06-10 17:14:04 +0300

    add di files to repo and bloc

--- lib/features/home/data/data_source/data_home.dart
+++ lib/features/home/data/data_source/data_home.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:injectable/injectable.dart';
 import 'package:movies_scope/features/constant/my_string.dart';
 import 'package:movies_scope/features/home/data/models/models_home.dart';
 
+@injectable
 class DataHome {
   Future<List<MovieModel>> getMoviePlayingNow() async {
     final url = Uri(
@@ -12,7 +14,7 @@ class DataHome {
       path: '/3/movie/now_playing',
       queryParameters: {"api_key": MyString.apiKey},
     );
-    final response = await http.get(url);
+    final response = await http.get(url).timeout(Duration(seconds: 10));
     if (response.statusCode == 200) {
       final List<MovieModel> listMovie = [];
       for (var element in jsonDecode(response.body)) {
--- lib/features/home/data/repository/repo_home.dart
+++ lib/features/home/data/repository/repo_home.dart
@@ -1,10 +1,15 @@
 import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
 import 'package:movies_scope/features/home/data/data_source/data_home.dart';
 import 'package:movies_scope/features/home/data/models/models_home.dart';
 
+@injectable
 class RepoHome {
+  final DataHome data;
+
+  RepoHome({required this.data});
   Future<Either<String, List<MovieModel>>> repoPlayNow() async {
-    final result = await DataHome().getMoviePlayingNow();
+    final result = await data.getMoviePlayingNow();
     try {
       return Right(result);
     } catch (e) {
